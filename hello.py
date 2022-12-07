09b817575047162654ef3883f03343c5d54294ac
Tibor Stanko <tibor.stanko@zurich.com>
2022-12-07 10:21:01 +0100

    pridal som slovensky pozdrav

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
+import sys
+
+
+def slovak():
+    print("ahoj")
+
 
 def default():
     print("hello")
 
 
 def main():
-    default()
+    if sys.argv[1] == "sk":
+        slovak()
+    else:
+        default()
 
 
 if __name__ == "__main__":
